31d9412abcf0bcee43b09b613a690c7316c6fbdf
nathanie <nathanie@post.bgu.ac.il>
2016-09-21 22:27:08 +0200

    Add files via upload

--- initial_data_preprocessing_windward.R
+++ initial_data_preprocessing_windward.R
@@ -19,4 +19,14 @@ num_ves_by_label<-uni_labeled %>%
   group_by(type) %>%
   summarise(counts = length(ves_id))
 
+library(reshape2)
+ports_by_ves<- dcast(port_visits,port_visits$ves_id~port_visits$port_id,fun.aggregate = length)
+
+#library(CatEncoders)
+#ohenc<- OneHotEncoder.fit(as.data.frame(port_visits[,c(1,5)]))
+
 write.csv(uni_labeled,"unique_vessels.csv",row.names = F)
+write.csv(ports_by_ves,"ports_by_vessel.csv",row.names = F)
+set.seed(12345)
+h<- sample(nrow(ports_by_ves),2500)
+trdata<- ports_by_ves[]
